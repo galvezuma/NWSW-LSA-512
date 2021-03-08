4c5f759944fc72216128a0b41dce299208b55485
galvez <galvez@DESKTOP-0MGTG3U.localdomain>
2021-03-08 14:31:54 +0100

    First commit.

---
+++ NWSW-LSA-512/src/NWSW-LSA-512.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : NWSW-LSA-512.c
+ Author      : SGR
+ Version     :
+ Copyright   : Código abierto
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hola amiguitos!!!"); /* prints !!!Hola amiguitos!!! */
+	return EXIT_SUCCESS;
+}
